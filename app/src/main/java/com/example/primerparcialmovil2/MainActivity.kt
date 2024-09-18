de5265854e21d760d282d2854b425360f2261199
Pedro Ramirez <pedroluisramirezo@gmail.com>
2024-09-18 19:10:00 -0400

    First app from module 1 'Dice Roller' completed.

--- app/src/main/java/com/example/primerparcialmovil2/MainActivity.kt
+++ app/src/main/java/com/example/primerparcialmovil2/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.example.primerparcialmovil2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.primerparcialmovil2.roll_dice.RollDiceMainActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnDiceRoller : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponents()
+        initListeners()
+
+    }
+
+    private fun initListeners() {
+        btnDiceRoller.setOnClickListener {
+            val intent = Intent(this, RollDiceMainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initComponents() {
+        btnDiceRoller = findViewById(R.id.btnEnterDiceRoller)
     }
 }
---
+++ app/src/main/java/com/example/primerparcialmovil2/roll_dice/RollDiceMainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.primerparcialmovil2.roll_dice
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.primerparcialmovil2.R
+import org.w3c.dom.Text
+import java.util.Random
+
+
+class RollDiceMainActivity : AppCompatActivity() {
+    private lateinit var diceImageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_roll_dice_main)
+
+        val rollButton: Button = findViewById(R.id.btn_roll_button)
+        diceImageView = findViewById(R.id.ivDice)
+
+        rollButton.text = "Let's Roll!"
+
+        rollButton.setOnClickListener {
+            //Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        //val resultText : TextView = findViewById(R.id.tvDiceText)
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> {R.drawable.empty_dice}
+        }
+
+        diceImageView.setImageResource(drawableResource)
+
+        //resultText.text = randomInt.toString()
+    }
+}
