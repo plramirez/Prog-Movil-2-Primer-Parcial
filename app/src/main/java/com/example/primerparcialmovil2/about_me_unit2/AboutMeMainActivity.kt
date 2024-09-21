5cd963e49d772322d66f61b842225c192f5c2814
Pedro Ramirez <pedroluisramirezo@gmail.com>
2024-09-21 08:53:18 -0400

    advances on unit 2 activities

--- app/src/main/java/com/example/primerparcialmovil2/about_me_unit2/AboutMeMainActivity.kt
+++ app/src/main/java/com/example/primerparcialmovil2/about_me_unit2/AboutMeMainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.primerparcialmovil2.about_me_unit2
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +17,25 @@ class AboutMeMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_me_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
     }
+
+    private fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        //Hide de Keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
+
